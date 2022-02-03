b7a2e69b9eaffa17c9796f2badb8df6588b7f428
Jun Seung Hyun <jsh5408@naver.com>
2022-02-03 17:37:32 +0900

    Update 2108. 통계학.py

--- BAEKJOON/SILVER/4/2108. 통계학/2108. 통계학.py
+++ BAEKJOON/SILVER/4/2108. 통계학/2108. 통계학.py
@@ -1,36 +1,36 @@
 from sys import stdin
- import collections
- import math
+import collections
+import math
 
- N = int(stdin.readline())
- nums = [0] * N
+N = int(stdin.readline())
+nums = [0] * N
 
- for i in range(N):
-     nums[i] = int(stdin.readline())
+for i in range(N):
+    nums[i] = int(stdin.readline())
 
- nums.sort()
+nums.sort()
 
- # 산술평균
- print(round(sum(nums)/N))
+# 산술평균
+print(round(sum(nums)/N))
 
- # 중앙값
- print(nums[N//2])
+# 중앙값
+print(nums[N//2])
 
- # 최빈값
- n = collections.Counter(nums)
- m = 1
- for k,v in n.items():
-     if v > m:
-         m = v
- r = []
- for k,v in n.items():
-     if v == m:
-         m = v
-         r.append(k)
-         if len(r) == 2:
-             r.pop(0)
-             break
- print(r[0])
+# 최빈값
+n = collections.Counter(nums)
+m = 1
+for k,v in n.items():
+    if v > m:
+        m = v
+r = []
+for k,v in n.items():
+    if v == m:
+        m = v
+        r.append(k)
+        if len(r) == 2:
+            r.pop(0)
+            break
+print(r[0])
 
- # 범위
- print(nums[-1] - nums[0])
+# 범위
+print(nums[-1] - nums[0])
