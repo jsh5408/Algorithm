625cb0ba08c39ec691a1f1ba2c828d761c8ddbfb
Jun Seung Hyun <jsh5408@naver.com>
2022-05-28 23:21:51 +0900

    Add 14501

---
+++ BAEKJOON/SILVER/3/14501. 퇴사/14501. 퇴사.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(input())
+
+timeTable = [list(map(int,sys.stdin.readline().split())) for i in range(N)]
+
+dp = [0 for i in range(N+1)]
+
+for i in range(N-1,-1,-1):
+    if i + timeTable[i][0] > N:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(timeTable[i][1] + dp[i + timeTable[i][0]], dp[i+1])
+
+print(dp[0])
